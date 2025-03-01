4c92d9d9d044fd00f6c5629f92e538c2d293d354
Deba43 <debadatta016@gmail.com>
2025-03-01 19:38:43 +0530

    code

--- src/main/java/com/example/Volunteering_Platform/config/SecurityConfig.java
+++ src/main/java/com/example/Volunteering_Platform/config/SecurityConfig.java
@@ -16,6 +16,10 @@ public class SecurityConfig {
                         .requestMatchers("/addTask").permitAll()
                         .requestMatchers("/viewAllTasks").permitAll()
                         .requestMatchers("/searchTasks").permitAll()
+                        .requestMatchers("/getTaskByName/{title}").permitAll()
+                        .requestMatchers("/getTaskByLocation/{location}").permitAll()
+                        .requestMatchers("/getTaskByCategory/{category}").permitAll()
+                        .requestMatchers("/getTaskByDate/{eventDate}").permitAll()
                         .anyRequest().authenticated());
 
         return http.build();
